07e3f90bc36e1393bf9e8ee31b37059444d20188
Joshadjei1 <joshuaa18.adjei@gmail.com>
2021-10-06 18:47:18 -0400

    Added undraw function

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -43,7 +43,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 
   let currentPosition = 4
-  let current = theTetrominoes[0][0]
+  let currentRotation = 0
+
+  let random = Math.floor(Math.random()*theTetrominoes.length)
+  let current = theTetrominoes[random][currentRotation]
 
   function draw() {
     current.forEach(index => {
@@ -51,5 +54,9 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 
-  draw()
+  function undraw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.remove('tetromino') 
+    })
+  }
 })
